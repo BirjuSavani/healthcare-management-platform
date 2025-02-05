304a0c3f95c812050f53598a549cc9d9a9ab89c0
Birju Savani <birju.savani@ics-global.in>
2025-02-05 12:34:40 +0530

    fix: delete dist folder

--- dist/components/User/Review/reviewService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ReviewService {
-}
-exports.default = new ReviewService();
-//# sourceMappingURL=reviewService.js.map
--- dist/utils/helper/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = asyncHandler;
-function asyncHandler(fn) {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next); // Pass any errors to Express's error handler
-    };
-}
-//# sourceMappingURL=asyncHandler.js.map
